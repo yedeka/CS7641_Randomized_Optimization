1ade2c76f453151769c55e782de6b744d1214589
yedekar3 <Yogi#2606>
2021-03-12 03:35:14 -0700

    Adding nqueens problem with Random Hill Climbing algorithm.

--- main.py
+++ main.py
@@ -5,5 +5,8 @@ from nQueens.runnQueens import run_ro_nqueens
 if __name__ == '__main__':
     # Define initial state
     init_state = np.array([0, 1, 2, 3, 4, 5, 6, 7])
-    run_ro_nqueens(8, init_state)
-
+    first_iters_fitness = run_ro_nqueens(8, init_state,1000)
+    second_iters_fitness = run_ro_nqueens(8, init_state, 2000)
+    third_iters_fitness = run_ro_nqueens(8, init_state, 3000)
+    fourth_iters_fitness = run_ro_nqueens(8, init_state, 4000)
+    fifth_iters_fitness = run_ro_nqueens(8, init_state, 5000)
--- nQueens/runnQueens.py
+++ nQueens/runnQueens.py
@@ -1,14 +1,51 @@
+import six
+import sys
+
+sys.modules['sklearn.externals.six'] = six
 import mlrose
-import numpy as np
 
 from nQueens.nqueensUtil import provide_max_function
 
 
-def run_ro_nqueens(state_size, initial_state):
+def run_ro_nqueens(state_size, initial_state, max_iters):
     problem = mlrose.DiscreteOpt(length=state_size, fitness_fn=provide_max_function(),
                                  maximize=True, max_val=state_size)
     # Use randomized hill climbing for solution
-    best_state, best_fitness = mlrose.random_hill_climb(problem=problem, init_state=initial_state, max_iters=1000,
-                                                        random_state=50);
-    print('The best state found is: ', best_state)
-    print('The fitness at the best state is: ', best_fitness)
+    hcbest_state, hcbest_fitness = mlrose.random_hill_climb(problem=problem, init_state=initial_state, max_iters=max_iters);
+    print('results of Random Hill Climbing')
+    print('-------------------------------')
+    print('-------------------------------')
+    print('The best state found is: ', hcbest_state)
+    print('The fitness at the best state is: ', hcbest_fitness)
+    print('--------------------------------------------------')
+    print('results of Simulated Annealing')
+    print('-------------------------------')
+    print('-------------------------------')
+    # Use simulated annealing for solution
+    sabest_state, sabest_fitness = mlrose.simulated_annealing(problem, schedule=mlrose.ExpDecay(), init_state=initial_state, max_iters=max_iters);
+    print('The best state found is: ', sabest_state)
+    print('The fitness at the best state is: ', sabest_fitness)
+    print('--------------------------------------------------')
+    print('results of Genetic Algorithm')
+    print('-------------------------------')
+    print('-------------------------------')
+    # Use genetic algorithm for solution
+    gabest_state, gabest_fitness = mlrose.genetic_alg(problem, max_iters=max_iters);
+    print('The best state found is: ', gabest_state)
+    print('The fitness at the best state is: ', gabest_fitness)
+    print('--------------------------------------------------')
+    # Use mimc for solution
+    print('results of mimic')
+    print('-------------------------------')
+    print('-------------------------------')
+    mbest_state, mbest_fitness = mlrose.mimic(problem, max_iters=max_iters);
+    print('The best state found is: ', mbest_state)
+    print('The fitness at the best state is: ', mbest_fitness)
+    print('--------------------------------------------------')
+    return {
+        'hill_climbing_fitness': hcbest_fitness,
+        'simulated_annealing_fitness': sabest_fitness,
+        'genetic_algorithm_fitness': gabest_fitness,
+        'mimic_fitness': mbest_fitness
+    }
+
