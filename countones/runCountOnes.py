d68b0b76a14c28507093edf44bf532e1606f9c24
yedekar3 <Yogi#2606>
2021-03-13 14:56:16 -0700

    Adding more print statements for analysis purpose and finishing the work on the randomized optimization algorithms.

--- countones/runCountOnes.py
+++ countones/runCountOnes.py
@@ -17,24 +17,24 @@ def run_one_max(initial_state, vector_dimension):
     start = datetime.now()
     rhc_results, rhc_fitness = mlrose.random_hill_climb(problem=problem, init_state=initial_state, max_attempts=100, max_iters=1000);
     end = datetime.now()
-    results['RHC'] = [rhc_fitness, end-start]
+    results['RHC'] = [rhc_fitness, (end-start).total_seconds()]
 
     print("Running simulated annealing")
     start = datetime.now()
     sa_results, sa_fitness =  mlrose.simulated_annealing(problem, schedule=mlrose.ExpDecay(), init_state=initial_state, max_attempts=100, max_iters=1000);
     end = datetime.now()
-    results['SA'] = [sa_fitness, end-start]
+    results['SA'] = [sa_fitness, (end-start).total_seconds()]
 
     print("Running genetic algorithm")
     start = datetime.now()
     ga_results, ga_fitness =  mlrose.genetic_alg(problem, max_attempts=100, max_iters=1000);
     end = datetime.now()
-    results['GA'] = [ga_fitness, end-start]
+    results['GA'] = [ga_fitness, (end-start).total_seconds()]
 
     print("Running MIMIC")
     start = datetime.now()
     mimic_results, mimic_fitness = mlrose.mimic(problem, max_attempts=100, max_iters=1000);
     end = datetime.now()
-    results['MIMIC'] = [mimic_fitness, end - start]
+    results['MIMIC'] = [mimic_fitness, (end-start).total_seconds()]
 
     return results
