fce0645cbf74b068731564f9daca940b61b8b39a
yedekar3 <Yogi#2606>
2021-03-14 19:46:50 -0700

    Uncommenting the problem domain code.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from neuralNetwork.run_nn_analysis import run_plot_nn
 
 
 def perform_experiment():
-    #perform_ro_analysis()
+    perform_ro_analysis()
     run_plot_nn()
 
 if __name__ == '__main__':
